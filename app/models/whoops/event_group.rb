a8987e0f453e770a4224a0d63e13e62e2034e389
Michael Siebert <siebertm85@googlemail.com>
2013-11-28 08:51:49 +0100

    add some indexes to speed up operations

--- app/models/whoops/event_group.rb
+++ app/models/whoops/event_group.rb
@@ -2,7 +2,7 @@ class Whoops::EventGroup
   # notifier responsible for creating identifier from notice details
   include Mongoid::Document
   include FieldNames
-  
+
   [
     :service,
     :environment,
@@ -13,19 +13,24 @@ class Whoops::EventGroup
   ].each do |string_field|
     field string_field, :type => String
   end
-  
+
+  index :service
+  index :environment
+  index :event_type
+  index [:service, :environment, :event_type, :event_group_identifier]
+
   field :last_recorded_at, :type => DateTime
   field :archived, :type => Boolean, :default => false
   field :event_count, :type => Integer, :default => 0
-  
+
   has_many :events, :class_name => "Whoops::Event"
-  
+
   validates_presence_of :event_group_identifier, :event_type, :service, :message
-  
+
   def self.identifying_fields
     field_names - ["message", "last_recorded_at"]
   end
-  
+
   # @return sorted set of all applicable namespaces
   def self.services
     all.distinct(:service).sort
